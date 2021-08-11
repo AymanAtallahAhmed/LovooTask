07f4d4651fac1726ce50814c40f3f6070fb980dc
Ayman <princeofknights22@gmail.com>
2021-08-11 14:38:03 +0200

    fixed high memory usage

--- LovooTask/Extensions/UIImageView+Extensions.swift
+++ LovooTask/Extensions/UIImageView+Extensions.swift
@@ -11,28 +11,43 @@ import RxAlamofire
 
 extension UIImageView {
     
-    private var cache: NSCache<NSString, UIImage>? {
-        NSCache<NSString, UIImage>()
-    }
-    
     func setImageWith(url: String) {
-        guard let URL = URL(string: url) else { return }
+        guard let imageURL = URL(string: url) else { return }
         
-        let cacheKey = NSString(string: url)
-        if let image = self.cache?.object(forKey: cacheKey) {
-            self.image = image
-            return
+        var cgImage: CGImage?
+        let backgroundQueue: DispatchQueue = .init(label: "Lovoo")
+        let maxDimentionInPixels = max(bounds.size.width,
+                                       bounds.size.height) * UIScreen.main.scale
+        let backgroundWorkItem = DispatchWorkItem.init(qos: .userInteractive, flags: .assignCurrentContext) { [weak self] in
+            self?.getCGImageFor(url: imageURL, dimention: maxDimentionInPixels) { image in
+                cgImage = image
+            }
         }
         
-        _ = data(.get, URL)
-            .subscribe(onNext: { [weak self] (data) in
-                if let imageData = UIImage(data: data)?.jpegData(compressionQuality: 0.002) {
-                    if let compressedImage = UIImage(data: imageData) {
-                        self?.cache?.setObject(compressedImage, forKey: cacheKey)
-                        self?.image = compressedImage
-                    }
-                }
-            })
+        backgroundWorkItem.notify(queue: .main) { [weak self] in
+            guard let self = self,
+                let cgImage = cgImage else { return }
+            self.image = UIImage.init(cgImage: cgImage)
+        }
+        
+        backgroundQueue.async(execute: backgroundWorkItem)
+    }
+    
+    
+    private func getCGImageFor(url: URL, dimention: CGFloat, completion: @escaping (CGImage) -> Void) {
+        let imageSourceOptions = [kCGImageSourceShouldCache: true] as CFDictionary
+        guard let imageSource = CGImageSourceCreateWithURL(url as CFURL, imageSourceOptions) else { return }
+        
+        let downSampledOptions = [
+            kCGImageSourceCreateThumbnailFromImageIfAbsent: true,
+            kCGImageSourceCreateThumbnailFromImageAlways: true,
+            kCGImageSourceShouldCache: true,
+            kCGImageSourceCreateThumbnailWithTransform: true,
+            kCGImageSourceThumbnailMaxPixelSize: dimention*1.4
+            ] as CFDictionary
+        
+        guard let downSampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downSampledOptions) else { return }
+        completion(downSampledImage)
     }
     
     func enableZoom() {
